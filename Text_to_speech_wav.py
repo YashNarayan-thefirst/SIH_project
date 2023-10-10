c6ef1ac9936d0aac2b391b85454503acafe08428
YashNarayan-thefirst <106808693+YashNarayan-thefirst@users.noreply.github.com>
2023-10-10 22:36:42 +0530

    Delete Text_to_speech_wav.py

--- Text_to_speech_wav.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-
-def tts(text):
-    # Save the text-to-speech output as a WAV file
-    tts_output = gTTS(text, lang='en', tld='us')
-    tts_output.save('sample.wav')
-txt = input("Enter text: ")
-tts(txt)
